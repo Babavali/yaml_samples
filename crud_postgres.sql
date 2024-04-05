4a90318a0950be3d2812137c8da08431e34d6526
Babavali <valibab@gmail.com>
2024-04-05 21:22:48 +0530

    Update crud_postgres.sql

--- crud_postgres.sql
+++ crud_postgres.sql
@@ -16,7 +16,7 @@ CREATE TABLE `address` (
   `pincode` varchar(255) DEFAULT NULL,
   `state` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) 
 
 
 
