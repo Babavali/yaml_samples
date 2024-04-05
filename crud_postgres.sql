6192e5ebf63369efb6d98605a2bb94359703958e
Babavali <valibab@gmail.com>
2024-04-05 21:21:07 +0530

    Update crud_postgres.sql

--- crud_postgres.sql
+++ crud_postgres.sql
@@ -4,6 +4,22 @@ USE `postgres`;
 
 -- Drop table
 
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `address` (
+  `id` varchar(255) NOT NULL,
+  `address_line1` varchar(255) DEFAULT NULL,
+  `address_line2` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `pincode` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
 DROP TABLE IF EXISTS `id_proof`;
 
 CREATE TABLE 'id_proof' (
