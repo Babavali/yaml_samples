0c7627eeef119d2cb2c7e3be977617113e03ae6b
Babavali <valibab@gmail.com>
2024-04-05 21:32:08 +0530

    Update crud_postgres.sql

--- crud_postgres.sql
+++ crud_postgres.sql
@@ -4,6 +4,31 @@ USE `postgres`;
 
 -- Drop table
 
+CREATE TABLE `address` (
+  `id` varchar(255) NOT NULL,
+  `address_line1` varchar(255) DEFAULT NULL,
+  `address_line2` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `pincode` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `id` int NOT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_hayqfjiruee1h6gs8jpcnjkmh` (`address_id`),
+  CONSTRAINT `fklm8wihj7bmjdv4lyk18e9j814` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`)
+)
+
 DROP TABLE IF EXISTS `id_proof`;
 
 CREATE TABLE 'id_proof' (
