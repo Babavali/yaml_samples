f4887f176138146ca98c4696fc4258661164cf19
Babavali <valibab@gmail.com>
2024-04-09 16:51:43 +0530

    Update Dump20240327_mysql.sql

--- Dump20240327_mysql.sql
+++ Dump20240327_mysql.sql
@@ -36,10 +36,10 @@ CREATE TABLE `address_map` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `employee_test`;
+DROP TABLE IF EXISTS `employee_map`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `employee_test` (
+CREATE TABLE `employee_map` (
   `id` int NOT NULL,
   `city` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
