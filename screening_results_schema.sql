2854a76133e3c7355cc0c99ea9053fc3170f4397
Babavali <valibab@gmail.com>
2024-08-12 09:57:05 +0530

    Update screening_results_schema.sql

--- screening_results_schema.sql
+++ screening_results_schema.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE  IF NOT EXISTS `postgres`
 USE `postgres`;
 
-DROP TABLE IF EXISTS `screening_results`;
-CREATE TABLE `screening_results` (
+DROP TABLE IF EXISTS `FINX_CA_ENTITES_SCAN`;
+CREATE TABLE `FINX_CA_ENTITES_SCAN` (
     `CA_workflow_instance_identifier` VARCHAR(500) NOT NULL,
     `CA_screening_configuration_identifier` VARCHAR(500) NOT NULL,
     `MSD_entity_id` VARCHAR(255),
-    `MSD_entity_type` VARCHAR(100),
+    `MSD_entity_type` VARCHAR(100) NOT NULL CHECK (`MSD_entity_type` IN ('corporate', 'individual')),
     `Finx_entity_status` VARCHAR(100),
     `Finx_Api_id` VARCHAR(100),
     `Finx_overall_decision` VARCHAR(100),
@@ -14,6 +14,6 @@ CREATE TABLE `screening_results` (
     `scan_initiated_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `scan_status_updated_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `CA_Webhook_configuration_identifier` VARCHAR(500),
-    PRIMARY KEY (`ID SERIAL`),
+    PRIMARY KEY (`ID`),
 )
 
