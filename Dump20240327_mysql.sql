d0c5f45c44705882bfa05a17f39bc3b3aafe9094
Babavali <valibab@gmail.com>
2024-09-03 13:48:17 +0530

    Update Dump20240327_mysql.sql

--- Dump20240327_mysql.sql
+++ Dump20240327_mysql.sql
@@ -1,15 +1,14 @@
 CREATE DATABASE  IF NOT EXISTS `rest_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `rest_db`;
 
-drop table if exists table `report_access_log`;
-	
+DROP TABLE IF EXISTS `report_access_log`;
 CREATE TABLE `report_access_log` (
-`id` varchar(255) NOT NULL,
-`report_name` varchar (64),
-`parameters` varchar (256),
-`user_name` varchar (64),
-`generated_timestamp`	date
-PRIMARY KEY (`id`)
+ `id` varchar(255) NOT NULL,
+ `report_name` varchar (64),
+ `parameters` varchar (256),
+ `user_name` varchar (64),
+ `generated_timestamp` date
+ PRIMARY KEY (`id`)
 );
 
 DROP TABLE IF EXISTS `address_map`;
