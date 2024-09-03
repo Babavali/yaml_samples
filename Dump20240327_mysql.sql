8a1551643f3db394155e0cfa8028fc02483f609e
Babavali <valibab@gmail.com>
2024-09-03 13:37:53 +0530

    Update Dump20240327_mysql.sql

--- Dump20240327_mysql.sql
+++ Dump20240327_mysql.sql
@@ -4,6 +4,7 @@ USE `rest_db`;
 drop table if exists table `report_access_log`;
 	
 create table `report_access_log` (
+	 `id` varchar(255) NOT NULL,
 	`report_name` varchar (64),
 	`parameters` varchar (256),
 	`user_name` varchar (64),
